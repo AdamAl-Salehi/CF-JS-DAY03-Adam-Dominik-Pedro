8ad5ecf9d42f1f1a55ff536aa83edacf5be13ffa
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-09-22 11:26:38 +0200

    basic5 done

---
+++ basic.js
@@ -0,0 +1,6 @@
+function formas(width, height, depth){
+	var area = width * height ;
+	var vol = width * height * depth ; 
+	document.write("The area of the box is:"+area+"<br> The volume of the box is:" +vol);
+}
+formas(10, 10, 20);
