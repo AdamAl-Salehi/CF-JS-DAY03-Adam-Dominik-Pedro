21c423a5007b1b6db10ff0d2b149bd16fceb062a
Ighile Pedro <esevee@yahoo.com>
2020-09-22 14:05:11 +0200

    advanced1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-function ageCalculator(currentYear,birthYear){
-    return (currentYear - birthYear);
+// function ageCalculator(currentYear,birthYear){
+//     return (currentYear - birthYear);
+
+// }
+// var age1 = ageCalculator(2020,1992);
+// var age2 = ageCalculator(2020,1992)+1;
+
+// console.log("you are either " + age1 + " or " + age2 );
+
+
+
+
+//Advance 1
+
+function timeCovert (n) {
+    var num = n;
+    var hours = Math.floor (num / 60);
+    // var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.floor(minutes);
+    return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
 
 }
-var age1 = ageCalculator(2020,1992);
-var age2 = ageCalculator(2020,1992)+1;
 
-console.log("you are either " + age1 + " or " + age2 );
+console.log(timeCovert(300));
