0554ee411c28dfd58fd936adb172342c27d9e1c2
Dominik Ringbauer <dominik.ringbauer@gmx.at>
2020-09-22 14:38:19 +0200

    advanced 2 added

--- Advanced2.js
+++
@@ -1,8 +0,0 @@
-function bgc (red, green, blue){
-	var red = Math.floor(Math.random()*256);
-	var green = Math.floor(Math.random()*256);
-	var blue = Math.floor(Math.random()*256);
-	document.getElementsByTagName("BODY")[0].style.background =  'rgb(' + red + ',' + green + ',' + blue + ')';
-}
-bgc();
-
